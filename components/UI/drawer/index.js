fcaeed1933c1612f5172193408ad075a94aa5860
Ahmed Kamran <ahmed.amigo3@gmail.com>
2023-11-17 14:06:29 +0500

    completed drawer

--- components/UI/drawer/index.js
+++ components/UI/drawer/index.js
@@ -1,18 +1,72 @@
+import { Drawer, IconButton, ListItemButton, ListItemText, styled } from '@mui/material';
+
 import {
   DASHBOARD_DRAWER_FULLWIDTH,
   DASHBOARD_DRAWER_RESPONSIVEWIDTH,
   NAV_HEIGHT,
 } from '@/utils/constants';
-import { Box, styled } from '@mui/material';
 
-export const CustomDrawer = styled(Box)(({ theme, selected }) => ({
-  height: `calc(100% - ${NAV_HEIGHT}px)`,
-  border: '1px solid black',
-  width: `${DASHBOARD_DRAWER_FULLWIDTH}px`,
-  position: 'absolute',
-  overflow: 'hidden',
+export const CustomDrawer = styled(Drawer)(({ theme, open }) => ({
+  '& .MuiDrawer-paper': {
+    color: 'black',
+    width: `${DASHBOARD_DRAWER_FULLWIDTH}px`,
+    zIndex: 1,
+    position: 'absolute',
+    marginTop: `${NAV_HEIGHT}px`,
+    height: `calc(100% - ${NAV_HEIGHT}px)`,
+    boxShadow: '2px 0px 10px lightgrey',
+    overflowX: 'hidden',
+  },
 
   [theme.breakpoints.down('md')]: {
-    width: `${DASHBOARD_DRAWER_RESPONSIVEWIDTH}px`,
+    '& .MuiDrawer-paper': {
+      width: open ? `65vw` : `${DASHBOARD_DRAWER_RESPONSIVEWIDTH}px`,
+      transition: 'all 0.5s',
+    },
+  },
+}));
+
+export const DrawerIcon = styled(IconButton)(({ theme, open }) => ({
+  marginLeft: open && 'auto',
+  marginRight: open && `${theme.spacing(2)}`,
+  display: 'none',
+  [theme.breakpoints.down('md')]: {
+    display: 'block',
+  },
+}));
+
+export const DrawerListText = styled(ListItemText)(({ theme, open }) => ({
+  whiteSpace: 'nowrap',
+  textOverflow: 'ellipsis',
+  [theme.breakpoints.down('md')]: {
+    display: open ? 'block' : 'none',
+  },
+}));
+
+export const DrawerListButton = styled(ListItemButton)(({ theme }) => ({
+  paddingLeft: `${theme.spacing(5)}`,
+  borderRadius: '15px',
+  height: '45px',
+
+  '&:hover': {
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.static.primary,
+    '& .MuiListItemIcon-root': {
+      color: theme.palette.static.primary,
+    },
+  },
+
+  '&.Mui-selected': {
+    backgroundColor: theme.palette.secondary.main,
+    color: theme.palette.static.primary,
+    '& .MuiListItemIcon-root': {
+      color: theme.palette.static.primary,
+    },
+  },
+
+  '&.Mui-selected:hover': { backgroundColor: theme.palette.secondary.main },
+
+  [theme.breakpoints.down('md')]: {
+    paddingLeft: `${theme.spacing(1)}`,
   },
 }));
--- components/layout/restaurant-dashboard-layout.js
+++ components/layout/restaurant-dashboard-layout.js
@@ -1,13 +1,99 @@
-import { Box, Drawer } from '@mui/material';
-import React from 'react';
-import { CustomDrawer } from '../UI';
+import React, { useState } from 'react';
+import Link from 'next/link';
+
+//Styles
+import { List, ListItem, ListItemIcon } from '@mui/material';
+import { DrawerIcon, CustomDrawer, DrawerListText, DrawerListButton } from '../UI';
+
+//Icons
+import SummarizeIcon from '@mui/icons-material/Summarize';
+import EditNoteIcon from '@mui/icons-material/EditNote';
+import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
+import ReviewsIcon from '@mui/icons-material/Reviews';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+import WorkHistoryIcon from '@mui/icons-material/WorkHistory';
+import LocalPoliceIcon from '@mui/icons-material/LocalPolice';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import RestaurantLogo from '../restaurant-dashboard/restaurant-logo/restaurant-logo';
 
 const RestaurantDashboardLayout = ({ children }) => {
+  const [selectedPage, setSelectedPage] = useState(0);
+  const [open, setOpen] = useState(false);
+
+  const id = 1;
+
+  const dashboardLinks = [
+    {
+      id: 'Overview',
+      link: `/restaurant/dashboard/${id}/overview`,
+      icon: <SummarizeIcon />,
+    },
+    {
+      id: 'Edit Details',
+      link: `/restaurant/dashboard/${id}/edit-details`,
+      icon: <EditNoteIcon />,
+    },
+    {
+      id: 'Feature History',
+      link: `/restaurant/dashboard/${id}/feature-history`,
+      icon: <MonetizationOnIcon />,
+    },
+    {
+      id: 'Reviews',
+      link: `/restaurant/dashboard/${id}/reviews`,
+      icon: <ReviewsIcon />,
+    },
+    {
+      id: 'Menu',
+      link: `/restaurant/dashboard/${id}/menu`,
+      icon: <MenuBookIcon />,
+    },
+    {
+      id: 'Logs',
+      link: `/restaurant/dashboard/${id}/logs`,
+      icon: <WorkHistoryIcon />,
+    },
+    {
+      id: 'Badges',
+      link: `/restaurant/dashboard/${id}/badges`,
+      icon: <LocalPoliceIcon color="inherit" />,
+    },
+  ];
+
+  const handleNavDrawer = () => {
+    setOpen((prevState) => !prevState);
+  };
+
   return (
-    <>
-      <CustomDrawer>halo</CustomDrawer>
+    <React.Fragment>
+      <CustomDrawer variant="permanent" open={open} onClose={handleNavDrawer}>
+        <DrawerIcon onClick={handleNavDrawer} open={open}>
+          {open ? (
+            <KeyboardArrowLeftIcon color="primary" fontSize="large" />
+          ) : (
+            <KeyboardArrowRightIcon color="primary" fontSize="large" />
+          )}
+        </DrawerIcon>
+        <RestaurantLogo open={open} />
+        <List>
+          {dashboardLinks.map((item, index) => (
+            <Link href={item.link} key={item.id}>
+              <ListItem>
+                <DrawerListButton
+                  selected={selectedPage === index}
+                  onClick={() => setSelectedPage(index)}
+                >
+                  <ListItemIcon>{item.icon}</ListItemIcon>
+                  <DrawerListText primary={item.id} open={open} />
+                </DrawerListButton>
+              </ListItem>
+            </Link>
+          ))}
+        </List>
+      </CustomDrawer>
       {children}
-    </>
+    </React.Fragment>
   );
 };
 
