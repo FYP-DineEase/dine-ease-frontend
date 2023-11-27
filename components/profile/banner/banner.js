4170fc1d03075e67c0e8fb69da434eb136db0b54
Ahmed Kamran <ahmed.amigo3@gmail.com>
2023-11-27 20:54:21 +0500

    added option for updating banner

--- components/profile/banner/banner.js
+++ components/profile/banner/banner.js
@@ -1,16 +1,57 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Image from 'next/image';
+import ProfileContext from '@/store/profile-context/profile-context';
+
+//Styles
+import { Box, Button } from '@mui/material';
+import { Text } from '@/components/UI';
 import { BannerContainer } from './banner.styles';
 
+//Snackbar
+import { enqueueSnackbar } from 'notistack';
+
+import userImage from '@/public/assets/images/avatar.jpg';
+
 const Banner = () => {
+  const ProfileCtx = useContext(ProfileContext);
+  const banner = ProfileCtx.background;
+
+  const handleConfirmBanner = () => {
+    enqueueSnackbar({ variant: 'success', message: 'Banner Updated Successfully!' });
+    ProfileCtx.backgroundConfirmationHandler(false);
+  };
+  const handleCancelBanner = () => {
+    ProfileCtx.backgroundConfirmationHandler(false);
+    ProfileCtx.profileBackgroundHandler(ProfileCtx.oldBackground);
+  };
+
   return (
     <BannerContainer>
       <Image
-        src={'/assets/images/background.jpg'}
+        src={banner || userImage.src}
         fill={true}
         objectFit="cover"
         alt="profile-background"
       />
+      <Box sx={{ position: 'absolute', top: 15, right: 15 }}>
+        {ProfileCtx.backgroundConfirmation && (
+          <React.Fragment>
+            <Button
+              variant="contained"
+              color="success"
+              sx={{ mr: 1 }}
+              onClick={handleConfirmBanner}
+            >
+              <Text variant="body">Save Changes</Text>
+            </Button>
+            <Button variant="contained" color="error" onClick={handleCancelBanner}>
+              <Text variant="body" color="text.primary">
+                Cancel
+              </Text>
+            </Button>
+          </React.Fragment>
+        )}
+      </Box>
     </BannerContainer>
   );
 };
