4815217c0da8798afe6981c8fb72d5a7cf2da94c
Ahmed Kamran <ahmed.amigo3@gmail.com>
2023-12-04 14:40:44 +0500

    renamed

---
+++ pages/email-confirmation.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import EmailConfirmation from '@/components/email-confirmation/email-confirmation';
+
+const EmailConfirmationPage = () => {
+  return <EmailConfirmation />;
+};
+
+export default EmailConfirmationPage;
