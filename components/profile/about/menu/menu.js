17c2376dcd776beb9beb96930e178cb0237ae400
Ahmed Kamran <ahmed.amigo3@gmail.com>
2023-12-18 00:46:51 +0500

    added page title

--- components/profile/about/menu/menu.js
+++ components/profile/about/menu/menu.js
@@ -55,6 +55,26 @@ const EditProfileMenu = ({ handleShowModal, handleAvatar }) => {
         onClose={closeMenu}
         transformOrigin={{ horizontal: 'center', vertical: 'top' }}
         anchorOrigin={{ horizontal: 'center', vertical: 'bottom' }}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: 'visible',
+            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            mt: 1.5,
+            '&:before': {
+              content: '""',
+              display: 'block',
+              position: 'absolute',
+              top: 0,
+              right: '50%',
+              width: 10,
+              height: 10,
+              bgcolor: 'background.paper',
+              transform: 'translateY(-50%) rotate(45deg)',
+              zIndex: 0,
+            },
+          },
+        }}
         TransitionComponent={Fade}
       >
         <MenuItem
