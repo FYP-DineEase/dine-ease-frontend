c5df23f8dc058d34a36c2aa2ab07502d6077f53d
Ahmed Kamran <ahmed.amigo3@gmail.com>
2023-12-18 00:55:36 +0500

    completed functionalities + adjusted styling and design

--- components/restaurant-dashboard/restaurant-images/restaurant-images.js
+++ components/restaurant-dashboard/restaurant-images/restaurant-images.js
@@ -2,28 +2,57 @@ import React, { useState } from 'react';
 
 // Styles
 import * as Styles from './restaurant-images.styles';
-import { DashboardContainer, DashboardContent, Text } from '@/components/UI';
-import { Button, Grid, ImageList, ImageListItem, Input, Tooltip } from '@mui/material';
+import {
+  DashboardContainer,
+  DashboardContent,
+  FlexContainer,
+  PrimaryButton,
+  Text,
+} from '@/components/UI';
+import {
+  Grid,
+  IconButton,
+  ImageList,
+  Input,
+  Tooltip,
+  useMediaQuery,
+} from '@mui/material';
+
+// Utils
+import { validateFiles } from '@/utils/validateFiles';
 
 // Icons
 import UploadFile from '@mui/icons-material/UploadFile';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import Delete from '@mui/icons-material/Delete';
+import CloseIcon from '@mui/icons-material/Close';
 
 // Snackbar
 import { enqueueSnackbar } from 'notistack';
 
+//Modals
 import PreviewModal from './preview-modal/preview-modal';
+import DeleteModal from '@/components/modal/delete-modal/delete-modal';
 
 const RestaurantImages = () => {
   const [images, setImages] = useState([]);
+  const [selectedImages, setSelectedImages] = useState([]);
   const [previewImages, setPreviewImages] = useState([]);
-  const [showModal, setShowModal] = useState(false);
+  const [showPreviewModal, setShowPreviewModal] = useState(false);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const isMobile = useMediaQuery((theme) => theme.breakpoints.down('md'));
 
   const checkImageCount = () => {
     if (images.length + previewImages.length > 10) {
       enqueueSnackbar({
         variant: 'error',
         message: 'Total 10 Images can be featured at a time',
+      });
+      return false;
+    } else if (previewImages.length === 0) {
+      enqueueSnackbar({
+        variant: 'error',
+        message: 'Upload atleast 1 image',
       });
       return false;
     } else {
@@ -35,30 +64,41 @@ const RestaurantImages = () => {
     }
   };
 
-  const handleShowModal = () => {
-    setShowModal(true);
+  const handleShowDeleteModal = () => {
+    setShowDeleteModal((prevState) => !prevState);
   };
 
-  const handleCloseModal = () => {
-    setShowModal(false);
+  const handleShowPreviewModal = () => {
+    setShowPreviewModal(true);
+  };
+
+  const handleClosePreviewModal = () => {
+    setShowPreviewModal(false);
   };
 
   const previewImageSaveHandler = () => {
     if (checkImageCount()) {
       setImages((prevState) => [...prevState, ...previewImages]);
-      setShowModal(false);
+      setShowPreviewModal(false);
       setPreviewImages([]);
     }
   };
 
   const previewImageUploadHandler = (event) => {
     const selectedImagesFiles = event.target.files;
-    const selectedImages = Array.from(selectedImagesFiles).map((file) =>
-      URL.createObjectURL(file)
-    );
-    setPreviewImages((prevState) => [...prevState, ...selectedImages]);
-    event.target.value = '';
-    handleShowModal();
+    if (validateFiles(selectedImagesFiles)) {
+      const selectedImages = Array.from(selectedImagesFiles).map((file) =>
+        URL.createObjectURL(file)
+      );
+      setPreviewImages((prevState) => [...prevState, ...selectedImages]);
+      event.target.value = '';
+      handleShowPreviewModal();
+    } else {
+      enqueueSnackbar({
+        variant: 'error',
+        message: 'Invalid Extension Type',
+      });
+    }
   };
 
   const previewImageDeleteHandler = (index) => {
@@ -67,23 +107,44 @@ const RestaurantImages = () => {
     setPreviewImages(duplicatePreviewImages);
   };
 
+  const imageSelectionHandler = (index) => {
+    setSelectedImages((prevSelected) => {
+      if (prevSelected.includes(index)) {
+        return prevSelected.filter((prevIndex) => prevIndex !== index);
+      } else {
+        return [...prevSelected, index];
+      }
+    });
+  };
+
+  const imageDeletionHandler = () => {
+    const updatedImages = images.filter(
+      (image, index) => !selectedImages.includes(index)
+    );
+    setImages(updatedImages);
+    setSelectedImages([]);
+  };
+
+  const imageResetHandler = () => {
+    setSelectedImages([]);
+  };
+
   return (
     <React.Fragment>
       <PreviewModal
-        showModal={showModal}
-        handleCloseModal={handleCloseModal}
+        showModal={showPreviewModal}
+        handleCloseModal={handleClosePreviewModal}
         previewImages={previewImages}
         previewImageSaveHandler={previewImageSaveHandler}
         previewImageUploadHandler={previewImageUploadHandler}
         previewImageDeleteHandler={previewImageDeleteHandler}
       />
-
-      <DashboardContainer
-        container
-        minHeight="90vh"
-        justifyContent="center"
-        alignItems="center"
-      >
+      <DeleteModal
+        handleCloseModal={handleShowDeleteModal}
+        showModal={showDeleteModal}
+        handleDelete={imageDeletionHandler}
+      />
+      <DashboardContainer container>
         {!images.length ? (
           <Styles.ImagePlaceContainer>
             <Styles.ImagePlaceHolder
@@ -97,7 +158,7 @@ const RestaurantImages = () => {
               <Text variant="sub">Supported file types are: JPG, JPEG, PNG</Text>
               <Input
                 type="file"
-                inputProps={{ multiple: true }}
+                inputProps={{ multiple: true, accept: 'image/*' }}
                 sx={{ display: 'none' }}
               />
             </Styles.ImagePlaceHolder>
@@ -105,49 +166,71 @@ const RestaurantImages = () => {
         ) : (
           <Grid item xs={12}>
             <DashboardContent>
-              <Tooltip
-                open={images.length === 10}
-                title="Maximum 10 images allowed at a time. Delete some old images to upload new images."
-                placement="top"
+              <FlexContainer
+                sx={{ justifyContent: 'space-between', mb: 3, flexWrap: 'wrap' }}
               >
-                <Button
-                  component="label"
-                  variant="contained"
-                  startIcon={<UploadFile />}
-                  onChange={previewImageUploadHandler}
-                  disabled={images.length === 10}
+                <Text variant="subHeader" fontWeight={500}>
+                  Restaurant Images
+                </Text>
+                <Tooltip
+                  open={images.length === 10}
+                  title="Maximum 10 images allowed at a time. Delete some old images to upload new images."
+                  placement="top"
                 >
-                  Upload file
-                  <Input
-                    type="file"
-                    inputProps={{ multiple: true }}
-                    sx={{ display: 'none' }}
-                  />
-                </Button>
-              </Tooltip>
-              {previewImages.length > 0 && (
-                <Button variant="outlined" onClick={handleShowModal}>
-                  Preview Images
-                  <Input
-                    type="file"
-                    inputProps={{ multiple: true }}
-                    sx={{ display: 'none' }}
-                  />
-                </Button>
-              )}
-              <ImageList variant="masonry" cols={3} gap={8}>
-                {images.map((image, index) => (
-                  <ImageListItem key={index}>
-                    <img
-                      srcSet={`${image}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                      src={image}
-                      alt={image}
-                      loading="lazy"
-                      style={{ objectFit: 'cover' }}
+                  <PrimaryButton
+                    component="label"
+                    variant="contained"
+                    startIcon={<UploadFile />}
+                    onChange={previewImageUploadHandler}
+                    disabled={images.length === 10}
+                    sx={{ ml: 'auto' }}
+                  >
+                    <Text variant="body">Upload file</Text>
+                    <Input
+                      type="file"
+                      inputProps={{ multiple: true, accept: 'image/*' }}
+                      sx={{ display: 'none' }}
                     />
-                  </ImageListItem>
+                  </PrimaryButton>
+                </Tooltip>
+              </FlexContainer>
+              <ImageList
+                cols={isMobile ? 1 : 3}
+                gap={isMobile ? 10 : 20}
+                rowHeight={isMobile ? 200 : 425}
+                sx={{ maxHeight: isMobile ? '400px' : '875px' }}
+              >
+                {images.map((image, index) => (
+                  <Styles.StyledImageListItem
+                    key={index}
+                    selected={+selectedImages.includes(index)}
+                    onClick={() => imageSelectionHandler(index)}
+                  >
+                    <Styles.StyledImage
+                      src={image}
+                      alt="review-image"
+                      fill
+                      sizes="100vw"
+                      selected={+selectedImages.includes(index)}
+                    />
+                  </Styles.StyledImageListItem>
                 ))}
               </ImageList>
+              <Styles.DeletePopper open={selectedImages.length}>
+                <FlexContainer gap={1.5}>
+                  <IconButton onClick={imageResetHandler} color="primary">
+                    <Tooltip title="Reset Selected Images" placement="top" arrow>
+                      <CloseIcon />
+                    </Tooltip>
+                  </IconButton>
+                  <Text variant="body">{selectedImages.length} Selected Images</Text>
+                  <IconButton onClick={handleShowDeleteModal} color="error">
+                    <Tooltip title="Delete Selected Images" placement="top" arrow>
+                      <Delete />
+                    </Tooltip>
+                  </IconButton>
+                </FlexContainer>
+              </Styles.DeletePopper>
             </DashboardContent>
           </Grid>
         )}
--- components/restaurant-dashboard/restaurant-images/restaurant-images.styles.js
+++ components/restaurant-dashboard/restaurant-images/restaurant-images.styles.js
@@ -1,5 +1,6 @@
-import { Box, IconButton, styled } from '@mui/material';
+import { ImageListItem, styled } from '@mui/material';
 import { DashboardContent, FlexContainer } from '@/components/UI';
+import Image from 'next/image';
 
 export const ImagePlaceHolder = styled(FlexContainer)(({ theme }) => ({
   flexDirection: 'column',
@@ -8,10 +9,61 @@ export const ImagePlaceHolder = styled(FlexContainer)(({ theme }) => ({
   border: `1px dashed ${theme.palette.primary.main}`,
   cursor: 'pointer',
   borderRadius: 10,
+
+  [theme.breakpoints.down('md')]: {
+    gap: theme.spacing(2),
+    padding: theme.spacing(1.5),
+  },
 }));
 
 export const ImagePlaceContainer = styled(DashboardContent)(({ theme }) => ({
   width: '50%',
   padding: theme.spacing(4),
   borderRadius: 10,
+  margin: 'auto',
+  textAlign: 'center',
+
+  [theme.breakpoints.down('lg')]: {
+    width: '80%',
+    padding: theme.spacing(2),
+  },
+
+  [theme.breakpoints.down('md')]: {
+    width: '90%',
+  },
+}));
+
+export const StyledImage = styled(Image)(({ selected }) => ({
+  objectFit: 'cover',
+  borderRadius: '5px',
+  border: '1px solid transparent',
+  borderColor: selected && 'blue',
+}));
+
+export const StyledImageListItem = styled(ImageListItem)(({ selected }) => ({
+  cursor: 'pointer',
+  '&:before': {
+    content: `""`,
+    transition: 'all 0.5s',
+    borderRadius: '5px',
+    height: '100%',
+    width: '100%',
+    position: 'absolute',
+    top: '0',
+    zIndex: '1',
+    backgroundColor: selected && 'rgba(0, 42, 231, 0.247)',
+    opacity: '.5',
+  },
+  '&:hover:before': {
+    backgroundColor: 'rgba(0, 42, 231, 0.116)',
+  },
+}));
+
+export const DeletePopper = styled(DashboardContent)(({ open }) => ({
+  position: 'absolute',
+  bottom: 50,
+  left: '55%',
+  transform: 'translate(-50%, 0)',
+  transition: 'all 0.5s',
+  opacity: open ? 1 : 0,
 }));
