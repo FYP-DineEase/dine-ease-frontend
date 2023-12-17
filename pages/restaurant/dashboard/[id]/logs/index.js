28e54f306d64da5f6257d0b7916adaf247ad8df3
Ahmed Kamran <ahmed.amigo3@gmail.com>
2023-12-18 01:00:45 +0500

    initialized logs page

--- pages/restaurant/dashboard/[id]/logs/index.js
+++ pages/restaurant/dashboard/[id]/logs/index.js
@@ -1,12 +1,14 @@
-import RestaurantDashboardLayout from '@/components/layout/restaurant-dashboard-layout';
 import React from 'react';
 
+import RestaurantDashboardLayout from '@/components/layout/restaurant-dashboard-layout';
+import Logs from '@/components/restaurant-dashboard/logs/logs';
+
 const LogsPage = () => {
-  return <div>LogsPage</div>;
+  return <Logs />;
 };
 
 LogsPage.getLayout = (page) => {
-    return <RestaurantDashboardLayout>{page}</RestaurantDashboardLayout>;
-  };
+  return <RestaurantDashboardLayout>{page}</RestaurantDashboardLayout>;
+};
 
 export default LogsPage;
