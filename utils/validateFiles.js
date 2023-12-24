ca5b67860dae4719c92928de110f2006bcb9e08d
Ahmed Kamran <ahmed.amigo3@gmail.com>
2023-12-24 18:30:27 +0500

    added 1mb limitation for image

--- utils/validateFiles.js
+++ utils/validateFiles.js
@@ -3,12 +3,12 @@ export const validateFiles = (files) => {
 
   for (const file of files) {
     const fileExtension = file.name.split('.').at(-1).toLowerCase();
+    const fileSize = Math.round(file.size / 1024);
 
     if (!allowedFileTypes.includes(fileExtension)) {
-      return false;
-      //throw new  error
+      throw new Error('Invalid File Extension!');
+    } else if (fileSize > 1024) {
+      throw new Error('Image Size should be less than 1MB!');
     }
   }
-
-  return true;
 };
