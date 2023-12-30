bad2f0ccbf3920cec283762e8f335d45151ceb5c
AhmedKamran1 <ahmed.amigo3@gmail.com>
2023-12-30 17:47:07 +0500

    updated searching attributes

--- pages/search-restaurants.js
+++ pages/search-restaurants.js
@@ -5,6 +5,12 @@ import SearchRestaurant from '@/components/search-restaurant/search-restaurant';
 import { getApprovedRestaurants } from '@/services';
 import { connectToMeilisearch } from '@/services/meilisearch';
 
+function getRandomValue() {
+  const randomDecimal = Math.random();
+  const scaledValue = Math.floor(randomDecimal * 5) + 1;
+  return scaledValue;
+}
+
 const SearchPage = ({ restaurants }) => {
   return <SearchRestaurant restaurants={restaurants} />;
 };
@@ -12,16 +18,34 @@ const SearchPage = ({ restaurants }) => {
 export default SearchPage;
 
 export const getStaticProps = async () => {
-  const restaurants = await getApprovedRestaurants();
+  const restaurantsData = await getApprovedRestaurants();
+  const { restaurants } = restaurantsData.data;
+
+  const restaurantsDummy = restaurants.map((restaurantDetails) => ({
+    ...restaurantDetails,
+    rating: getRandomValue(),
+    reviewsCount: Math.floor(Math.random() * 1000),
+  }));
 
   const client = connectToMeilisearch();
+
   client
     .index('restaurants')
-    .addDocuments(restaurants.data, { primaryKey: 'id' })
+    .addDocuments(restaurantsDummy, { primaryKey: 'id' })
+    .catch((error) => console.error('MeiliSearch Error:', error));
+
+  client
+    .index('restaurants')
+    .updateFilterableAttributes(['cuisine'])
+    .catch((error) => console.error('MeiliSearch Error:', error));
+    
+  client
+    .index('restaurants')
+    .updateSortableAttributes(['rating', 'reviewsCount'])
     .catch((error) => console.error('MeiliSearch Error:', error));
 
   return {
-    props: { restaurants: restaurants.data },
+    props: { restaurants: restaurantsDummy },
     revalidate: 10,
   };
 };
