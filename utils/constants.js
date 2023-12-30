4129c93085e48870a841a54ff5fad59209367b4f
AhmedKamran1 <ahmed.amigo3@gmail.com>
2023-12-30 17:47:44 +0500

    added restaurant sorting constants

--- utils/constants.js
+++ utils/constants.js
@@ -3,3 +3,9 @@ export const DASHBOARD_DRAWER_FULLWIDTH = 350;
 export const DASHBOARD_DRAWER_RESPONSIVEWIDTH = 70;
 
 export const cuisineTypes = ['Mexican', 'Italian', 'Chinese', 'Fast Food', 'Sea Food'];
+
+export const sortBy = {
+  rating: 'Top Rated',
+  reviewsCount: 'Most Reviewed',
+  recommended: 'Recommended',
+};
