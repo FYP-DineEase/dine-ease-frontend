c76cc4904d97f9baf33d27e76b8cb83e0d96d212
AhmedKamran1 <ahmed.amigo3@gmail.com>
2024-01-19 22:16:17 +0500

    added otp modal

--- components/modal/otp-modal/otp-modal.js
+++ components/modal/otp-modal/otp-modal.js
@@ -2,13 +2,12 @@ import React, { useState } from 'react';
 
 // Styles
 import * as Styles from './otp-modal.styles';
-import { Button, Modal } from '@mui/material';
+import { Modal } from '@mui/material';
+import { ModalCancelIcon, PrimaryButton, Text } from '@/components/UI';
 
 // Icons
-import Delete from '@mui/icons-material/Delete';
-import { FlexContainer, PrimaryButton, Text } from '@/components/UI';
-import { MuiOtpInput } from 'mui-one-time-password-input';
-import Link from 'next/link';
+import MobileFriendlyIcon from '@mui/icons-material/MobileFriendly';
+import CloseIcon from '@mui/icons-material/Close';
 
 const OtpModal = ({ showModal, handleCloseModal }) => {
   const [otp, setOtp] = useState('');
@@ -20,28 +19,32 @@ const OtpModal = ({ showModal, handleCloseModal }) => {
   return (
     <Modal open={showModal}>
       <Styles.ModalContainer>
-        <MuiOtpInput
+        <ModalCancelIcon onClick={handleCloseModal}>
+          <CloseIcon color="secondary" fontSize="medium" />
+        </ModalCancelIcon>
+        <MobileFriendlyIcon sx={{ fontSize: '5rem' }} color="primary" />
+        <Text variant="main" fontWeight={800}>
+          Verify OTP
+        </Text>
+        <Text variant="body" textAlign="center">
+          An OTP has been sent to mobile number +923319555911
+        </Text>
+        <Styles.StyledOtp
           value={otp}
           onChange={handleChange}
+          gap={1}
           length={6}
           autoFocus
-          sx={{
-            input: {
-              color: 'text.secondary',
-            },
-          }}
         />
-        <FlexContainer sx={{ flexDirection: 'column' }}>
-          <PrimaryButton>
-            <Text variant="body">Verify</Text>
-          </PrimaryButton>
-          <Text variant="sub">
-            Did not recieve code?
-            <Text variant="sub" color="primary" ml={0.5}>
-              Resend OTP
-            </Text>
+        <PrimaryButton>
+          <Text variant="body">Verify</Text>
+        </PrimaryButton>
+        <Text variant="sub">
+          Did not recieve code?
+          <Text variant="sub" color="primary" ml={0.5}>
+            Resend OTP
           </Text>
-        </FlexContainer>
+        </Text>
       </Styles.ModalContainer>
     </Modal>
   );
