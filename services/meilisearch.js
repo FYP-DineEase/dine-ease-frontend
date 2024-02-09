50b436feb5d289a3de405c78f8b8f876d0df9150
Mujtaba Shafiq <mujtaba.shafiq213@gmail.com>
2024-02-09 22:34:09 +0500

    update

--- services/meilisearch.js
+++ services/meilisearch.js
@@ -1,10 +1,8 @@
 import { MeiliSearch } from 'meilisearch';
 
-const port = 7700;
-
 export const connectToMeilisearch = () => {
   const client = new MeiliSearch({
-    host: `http://localhost:${port}`,
+    host: `http://localhost:7700`,
     apiKey: 'cd13d088-21cf-4286-ae61-0643d321dd9e',
     headers: {
       Authorization: 'Bearer cd13d088-21cf-4286-ae61-0643d321dd9e',
