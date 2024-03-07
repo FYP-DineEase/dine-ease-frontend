7bc6e3db9048522f3db1befca0c497c1c001cf2f
Ahmed Kamran <ahmed.amigo3@gmail.com>
2024-03-07 15:05:46 +0500

    updated schema

--- utils/validation-schema/review.js
+++ utils/validation-schema/review.js
@@ -2,5 +2,5 @@ import * as yup from 'yup';
 
 export const reviewSchema = yup.object().shape({
   content: yup.string().required('Review content is required.'),
-  rating: yup.number().integer().positive().required('Rating is required'),
+  rating: yup.number().integer().required('Rating is required.'),
 });
