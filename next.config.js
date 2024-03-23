4d2e5f776c4274ee33b524c3f0db183826ee93e6
Ahmed Kamran <ahmed.amigo3@gmail.com>
2024-03-24 03:09:34 +0500

    added aws s3 reviews bucket

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
     domains: [
       `${process.env.NEXT_PUBLIC_AWS_S3_USERS_BUCKET}.s3.${process.env.NEXT_PUBLIC_AWS_S3_REGION}.amazonaws.com`,
       `${process.env.NEXT_PUBLIC_AWS_S3_RESTAURANTS_BUCKET}.s3.${process.env.NEXT_PUBLIC_AWS_S3_REGION}.amazonaws.com`,
+      `${process.env.NEXT_PUBLIC_AWS_S3_REVIEWS_BUCKET}.s3.${process.env.NEXT_PUBLIC_AWS_S3_REGION}.amazonaws.com`
     ],
   },
   compiler: {
